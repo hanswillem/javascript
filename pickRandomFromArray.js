84c35a088aadc27dd2c51418f521f8b2e2cfae4e
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2016-02-10 16:25:47 +0100

    Create pickRandomFromArray.js

---
+++ pickRandomFromArray.js
@@ -0,0 +1,11 @@
+// Picks a random item from an array, without ever picking the same item twice in a row
+
+myList = ['appels', 'peren', 'bananen'];
+
+function pickRandom(a) {
+    var r = Math.floor(Math.random() * (a.length - 1));
+    var item = a[r];
+    a.splice(r, 1);
+    a.push(item);
+    return item
+}
